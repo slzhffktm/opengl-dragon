48c80ab1118257cff902f82b3cf902f46bc4ce32
slzhffktm <pinkappleberry98@gmail.com>
2019-03-24 19:48:33 +0700

    prepare for draw dragon

--- main.cpp
+++ main.cpp
@@ -2,18 +2,17 @@
 #include <GL/glfw3.h>
 #include <stdlib.h>
 #include <iostream>
+
 using namespace std;
 
-void error_callback(int error, const char* description)
-{
+void error_callback(int error, const char *description) {
     fprintf(stderr, "Error: %s\n", description);
 }
 
-GLFWwindow* initialize_window() {
+GLFWwindow *initialize_window() {
     glewExperimental = true; // Needed for core profile
-    if( !glfwInit() )
-    {
-        fprintf( stderr, "Failed to initialize GLFW\n" );
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW\n");
     }
     glfwSetErrorCallback(error_callback);
 
@@ -24,14 +23,15 @@ GLFWwindow* initialize_window() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL
 
     // Open a window and create its OpenGL context
-    GLFWwindow* window; // (In the accompanying source code, this variable is global for simplicity)
+    GLFWwindow *window; // (In the accompanying source code, this variable is global for simplicity)
     window = glfwCreateWindow(1920, 1080, "Dragon", NULL, NULL);
-    if( window == NULL ){
-        fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+    if (window == NULL) {
+        fprintf(stderr,
+                "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
         glfwTerminate();
     }
     glfwMakeContextCurrent(window); // Initialize GLEW
-    glewExperimental=true; // Needed in core profile
+    glewExperimental = true; // Needed in core profile
     if (glewInit() != GLEW_OK) {
         fprintf(stderr, "Failed to initialize GLEW\n");
     }
@@ -45,7 +45,7 @@ void draw_dragon_right() {
     GLfloat g_vertex_buffer_data[] = {
             -1.0f, -1.0f, 0.0f,
             1.0f, -1.0f, 0.0f,
-            0.0f,  1.0f, 0.0f,
+            0.0f, 1.0f, 0.0f,
     };
 }
 
@@ -53,12 +53,13 @@ void draw_dragon() {
 
 }
 
-int main()
-{
-    GLFWwindow* window = initialize_window();
+int main() {
+    GLFWwindow *window = initialize_window();
 
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        draw_dragon();
 
         // Swap buffers
         glfwSwapBuffers(window);
