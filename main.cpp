f4e6038cb44ffa71993c46bc1b1aae27db8698c0
slzhffktm <pinkappleberry98@gmail.com>
2019-03-24 20:02:01 +0700

    add git ignore and add common folder for shader

--- main.cpp
+++ main.cpp
@@ -47,10 +47,33 @@ void draw_dragon_right() {
             1.0f, -1.0f, 0.0f,
             0.0f, 1.0f, 0.0f,
     };
+
+    // This will identify our vertex buffer
+    GLuint vertexBuffer;
+    // Generate 1 buffer, put the resulting identifier in vertexbuffer
+    glGenBuffers(1, &vertexBuffer);
+    // The following commands will talk about our 'vertexbuffer' buffer
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    // Give our vertices to OpenGL.
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+    // 1st attribute buffer : vertices
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glVertexAttribPointer(
+            0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+            3,                  // size
+            GL_FLOAT,           // type
+            GL_FALSE,           // normalized?
+            0,                  // stride
+            (void*)0            // array buffer offset
+    );
+    // Draw the triangle !
+    glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+    glDisableVertexAttribArray(0);
 }
 
 void draw_dragon() {
-
+    draw_dragon_right();
 }
 
 int main() {
